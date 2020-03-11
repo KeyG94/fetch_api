65716a1964d8bbf72ae41ea8faf9cbaae7f7b946
Glenn Key <glennkey78@live.no>
2020-03-11 19:04:55 +0100

    first initial commit

---
+++ scripts/fetch.js
@@ -0,0 +1,83 @@
+// refer to question 1 before development starts for scope document
+// connect to this api https://api.magicthegathering.io/v1/cards
+
+document.getElementById("get-btn").addEventListener("click", getText);
+document.getElementById('get-user').addEventListener("click", getUser);
+document.getElementById('get-api').addEventListener("click", getAPI);
+document.getElementById('addPost').addEventListener("submit", addPost);
+
+
+// function getText(){
+//     fetch('sample.txt')
+//     .then(function(response){
+//         return response.text()
+//     })
+//     .then(function(data){
+//         console.log(data)
+//     });
+// }
+
+// get data
+
+function getText() {
+    fetch("sample.txt")
+        .then(res => res.text())
+        .then(data => {
+            document.getElementById('output').innerHTML = data;
+        })
+        .catch(err => console.log(err));
+};
+
+// Getting json data and displaying it when button is clicked
+function getUser() {
+    fetch("user.json")
+        .then((res) => res.json())
+        .then((data) => {
+            let output = '<h2>User</h2>';
+            data.forEach(function (user) {
+                output += `
+            <ul>
+            <li>Name: ${user.name}</li>
+            <li>Age: ${user.age}</li>
+            <li>Car: ${user.car}</li>
+            </ul>
+            `
+            })
+            document.getElementById('output').innerHTML = output;
+        })
+};
+
+//Getting api data from a web server, then listing it
+
+function getAPI() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((res) => res.json())
+        .then((data) => {
+            let output = '<h2>Posts</h2>';
+            data.forEach(function (post) {
+                output += `        <div>
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+            </div>`
+            })
+            document.getElementById('output').innerHTML = output;
+        })
+};
+
+function addPost() {
+    e.preventDefault();
+
+    let title = document.getElementById('title').value;
+    let body = document.getElementById('body').value;
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*,',
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({ body: body, body: body })
+    })
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+};
